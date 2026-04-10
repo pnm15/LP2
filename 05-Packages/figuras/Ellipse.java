010dd15a04923612160f3cc4179fb17a4df868b3
pnm15 <pedro.neves.maia01@gmail.com>
2026-04-10 18:35:20 -0300

    Update Ellipse.java

--- 05-Packages/figuras/Ellipse.java
+++ 05-Packages/figuras/Ellipse.java
@@ -1,36 +1,27 @@
-package figuras;
+package figures;
 
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Ellipse2D.Double;
 
-class Ellipse {
+public class Ellipse {
     int x, y;
     int w, h;
-    int c1,c2,c3;
 
-    Ellipse (int x, int y, int w, int h,int c1,int c2,int c3) {
+    public Ellipse (int x, int y, int w, int h) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-	    this.c1=c1;
-	    this.c2=c2;
-	    this.c3=c3;
     }
 
-    void print () {
-        System.out.format("Elipse de tamanho (%d,%d) na posicao (%d,%d), Cor:(%d,%d,%d).\n",
-            this.w, this.h, this.x, this.y,this.c1,this.c2,this.c3);
+    public void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
     }
 
-    void paint (Graphics g) {
-          Graphics2D g2d = (Graphics2D) g;
-	      g2d.setColor(new Color(this.c1,this.c2,this.c3));
-	      g2d.fillOval(this.x,this.y, this.w,this.h);
-	      g2d.setPaint(Color.black);
-          g2d.drawOval(this.x,this.y, this.w,this.h);
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
     }
 }
