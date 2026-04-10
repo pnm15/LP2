6f2f65fae96be1be54ba200b620a3546c7d41087
pnm15 <pedro.neves.maia01@gmail.com>
2026-04-10 18:35:56 -0300

    Update Rect.java

--- 05-Packages/figuras/Rect.java
+++ 05-Packages/figuras/Rect.java
@@ -1,34 +1,25 @@
-package figuras;
+package figures;
 
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
 
-class Rect {
+public class Rect {
     int x, y;
     int w, h;
-    int c1,c2,c3;
-    Rect (int x, int y, int w, int h,int c1,int c2,int c3) {
+
+    public Rect (int x, int y, int w, int h) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-	      this.c1=c1;
-	      this.c2=c2;
-	      this.c3=c3;
     }
 
-    void print () {
-        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d), Cor:(%d,%d,%d).\n",
-            this.w, this.h, this.x, this.y,this.c1,this.c2,this.c3);
+    public void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
     }
 
-    void paint (Graphics g) {
+    public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-	g2d.setColor(new Color(this.c1,this.c2,this.c3));
-	g2d.fillRect(this.x,this.y, this.w,this.h);
-	g2d.setPaint(Color.black);
-	g2d.drawRect(this.x,this.y, this.w,this.h);
-	
+        g2d.drawRect(this.x,this.y, this.w,this.h);
     }
 }
