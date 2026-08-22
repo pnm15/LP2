0eeb6077302089eeb2be41b4eed61c5ede85a692
pnm15 <pedro.neves.maia01@gmail.com>
2026-08-22 17:03:27 -0300

    Create Figure.java

---
+++ Projetos/Projeto 1/figures/Figure.java
@@ -0,0 +1,24 @@
+package figuras;
+
+import ivisible.*;
+
+public abstract class Figure implements ivisible {
+    public int x, y;
+    public int w, h;
+
+    public Figure (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    public void drag (int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public boolean clicked (int x, int y) {
+        return (this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
+    }
+}
