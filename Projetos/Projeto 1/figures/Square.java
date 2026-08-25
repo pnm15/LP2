29c97e19538a752de71f4b04bd72066dbea63193
pnm15 <pedro.neves.maia01@gmail.com>
2026-08-25 19:27:09 -0300

    Create Square.java

---
+++ Projetos/Projeto 1/figures/Square.java
@@ -0,0 +1,21 @@
+package figures;
+import ivisible.*;
+import java.awt.*;
+
+public class Square extends Figure {
+    public Square (int x, int y, int w, int h, int c1, int c2, int c3) {
+        super(x,y,w,h,c1,c2,c3);
+    }
+    public void print () {
+        System.out.format("Quadrado de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y,this.c1,this.c2,this.c3);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+	g2d.setColor(new Color(this.c1,this.c2,this.c3));
+	g2d.fillRect(this.x,this.y, this.h,this.h);
+	g2d.setPaint(new Color(c3,c2,c1));
+	g2d.drawRect(this.x,this.y, this.h,this.h);
+    }
+}
