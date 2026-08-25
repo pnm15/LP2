441f478239ab0ee91e499d2e5fbcc5a767659737
pnm15 <pedro.neves.maia01@gmail.com>
2026-08-25 20:37:54 -0300

    Update IVisible.java

--- Projetos/Projeto 1/ivisible/IVisible.java
+++ Projetos/Projeto 1/ivisible/IVisible.java
@@ -2,7 +2,7 @@ package ivisible;
 
 import java.awt.Graphics;
 
-public interface ivisible {
+public interface IVisible {
     public boolean clicked (int x, int y);
     public void    paint   (Graphics g);
 }
