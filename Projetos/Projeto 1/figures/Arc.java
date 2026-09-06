6bd924602acf5bf141e2f016e69497f4935d489f
pnm15 <pedro.neves.maia01@gmail.com>
2026-09-06 10:58:32 -0300

    Create Arc.java

---
+++ Projetos/Projeto 1/figures/Arc.java
@@ -0,0 +1,26 @@
+package figures;
+
+import ivisible.*;
+import java.awt.*;
+
+
+public class Arc extends Figure {
+	public int ang1,ang2;
+    	public Arc (int x, int y, int w, int h,Color c,char k,int ang1,int ang2) {
+        super(x,y,w,h,c,k);
+	      this.ang1=ang1;
+	      this.ang2=ang2;
+    }
+
+    public void print () {
+        System.out.format("linha de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+	      g2d.setColor(this.c);
+   	    g2d.drawArc(this.x, this.y, this.w, this.h, this.ang1,this.ang2);
+	      g2d.fillArc(this.x, this.y, this.w, this.h, this.ang1,this.ang2);
+    }
+}
