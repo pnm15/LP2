deefdf674e4427f0795b4fc02d37909d8d7692da
pnm15 <pedro.neves.maia01@gmail.com>
2026-09-10 21:27:02 -0300

    Update Arc.java

--- Projetos/Projeto 1/figures/Arc.java
+++ Projetos/Projeto 1/figures/Arc.java
@@ -2,14 +2,14 @@ package figures;
 
 import ivisible.*;
 import java.awt.*;
-
+import java.awt.event.*;
 
 public class Arc extends Figure {
 	public int ang1,ang2;
-    	public Arc (int x, int y, int w, int h,Color c,char k,int ang1,int ang2) {
-        super(x,y,w,h,c,k);
-	      this.ang1=ang1;
-	      this.ang2=ang2;
+    	public Arc (int x, int y, int w, int h,Color c,int ang1,int ang2) {
+        super(x,y,w,h,c);
+	this.ang1=ang1;
+	this.ang2=ang2;
     }
 
     public void print () {
@@ -19,8 +19,8 @@ public class Arc extends Figure {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-	      g2d.setColor(this.c);
-   	    g2d.drawArc(this.x, this.y, this.w, this.h, this.ang1,this.ang2);
-	      g2d.fillArc(this.x, this.y, this.w, this.h, this.ang1,this.ang2);
+	g2d.setColor(this.c);
+   	g2d.drawArc(this.x, this.y, this.w, this.h, this.ang1,this.ang2);
+	g2d.fillArc(this.x, this.y, this.w, this.h, this.ang1,this.ang2);
     }
 }
