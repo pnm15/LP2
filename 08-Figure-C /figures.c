97c3ffb0fad0e01007ccea9e4ce3a6ef75f24485
pnm15 <pedro.neves.maia01@gmail.com>
2026-09-20 15:39:11 -0300

    Update figures.c

--- 08-Figure-C /figures.c
+++ 08-Figure-C /figures.c
@@ -7,12 +7,13 @@ typedef struct {
 
 struct Figure;
 typedef void (* Figure_Print) (struct Figure*);
+typedef void (* Figure_move) (struct Figure*,int,int);
 
 typedef struct Figure {
     int x, y;
     Color fg, bg;
     void (* print) (struct Figure*);
-    void (* move) (struct Figure*);
+    void (* move) (struct Figure*,int,int);
 } Figure;
 
 //metodos e construtor de figure
@@ -23,7 +24,7 @@ typedef struct {
 } Line;
 
 void Line_move (Line* this,int dx,int dy) {
-    Figure* sup = (Figure*) this
+    Figure* sup = (Figure*) this;
     sup->x+=dx;
     sup->y+=dy;
 }
@@ -38,19 +39,26 @@ Line* Line_new (int x, int y, int w, int h) {
     Line*   this  = malloc(sizeof(Line));
     Figure* sup = (Figure*) this;
     sup->print = (Figure_Print) Line_print;
+    sup->move = (Figure_move) Line_move;
     sup->x = x;
     sup->y = y;
     this->w = w;
     this->h = h;
 }
 
-//metodos e construtor de Line
+//metodos, metodos virtuais e construtor de Line
 
 typedef struct {
     Figure super;
     int w, h;
     int ang1, ang2;
 } Arc;
+
+void Arc_move (Arc* this,int dx,int dy) {
+    Figure* sup = (Figure*) this;
+    sup->x+=dx;
+    sup->y+=dy;
+}
 
 void Arc_print (Arc* this) {
     Figure* sup = (Figure*) this;
@@ -62,6 +70,7 @@ Arc* Arc_new (int x, int y, int w, int h,int ang1,int ang2) {
     Arc* this = malloc(sizeof(Arc));
     Figure* sup = (Figure*) this;
     sup->print = (Figure_Print) Arc_print;
+    sup->move = (Figure_move) Arc_move;
     sup->x = x;
     sup->y = y;
     this->w = w;
@@ -70,8 +79,8 @@ Arc* Arc_new (int x, int y, int w, int h,int ang1,int ang2) {
     this->ang2 = ang2;
 }
 
-//metodos e construtor de Arc
-void main (void) {
+//metodos, metodos virtuais e construtor de Arc
+void main () {
     Figure* figs[4] = {
         (Figure*) Line_new(10,10,100,100),
         (Figure*) Arc_new(40,10,140,300,0,340),
@@ -82,9 +91,11 @@ void main (void) {
 
     for (int i=0; i<4; i++) {
         figs[i]->print(figs[i]);
+         figs[i]->move(figs[i],10,20);
     }
 
-    //cria as figuras dentro de um vetor
+    //cria as figuras dentro de um vetor e utilizar metodos virtuais
+    
 
     for (int i=0; i<4; i++) {
         free(figs[i]);
