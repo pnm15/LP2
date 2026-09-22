221bdc1d2838be092f61690af30e124dde13a8bf
pnm15 <pedro.neves.maia01@gmail.com>
2026-09-22 16:52:36 -0300

    Update Rect.java

--- Projetos/Projeto 1/figures/Rect.java
+++ Projetos/Projeto 1/figures/Rect.java
@@ -11,11 +11,16 @@ public class Rect extends Figure {
             this.w, this.h, this.x, this.y,this.c);
     }
 
-    public void paint (Graphics g) {
+    public void paint (Graphics g,boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
 	g2d.setColor(this.c);
 	g2d.fillRect(this.x,this.y, this.w,this.h);
+    if (focused) {
+            g2d.setPaint(Color.RED);
+            g2d.fillRect(this.x-2,this.y-2, this.w+4,this.h+4);
+        }
 	g2d.setPaint(Color.BLACK);
 	g2d.drawRect(this.x,this.y, this.w,this.h);
+
     }
 }
