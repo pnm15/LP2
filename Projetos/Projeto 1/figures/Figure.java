7b98c27ff1626eb90feef0ed911cf77371fb8a1b
pnm15 <pedro.neves.maia01@gmail.com>
2026-09-22 16:50:00 -0300

    Update Figure.java

--- Projetos/Projeto 1/figures/Figure.java
+++ Projetos/Projeto 1/figures/Figure.java
@@ -2,9 +2,11 @@ package figures;
 import java.awt.Graphics;
 import java.awt.Color;
 import ivisible.*;
+
+import java.io.Serializable;
 import java.awt.event.*;
 
-public abstract class Figure implements IVisible {
+public abstract class Figure implements IVisible, Serializable {
     public int x, y;
     public int w, h;
     public Color c;
@@ -15,110 +17,107 @@ public abstract class Figure implements IVisible {
         this.y = y;
         this.w = w;
         this.h = h;
-	this.c=c;
+	    this.c=c;
     }
-    public void drag (Figure fig,MouseEvent evt,int cto,int dx,int dy) {
+    public void drag (MouseEvent evt,int cto,int dx,int dy) {
 		switch(cto) {
 			case 0:
-				fig.x = evt.getX()-dx;
-                                fig.y = evt.getY()-dy;
+				this.x = evt.getX()-dx;
+                               this.y = evt.getY()-dy;
 			break;
 
 			case 1:
-				if ((fig.w + fig.x -  evt.getX())>0) {
-					if ((fig.h + fig.y -  evt.getY())>0) {
-						fig.w += fig.x -  evt.getX();
-                                		fig.h += fig.y - evt.getY();
-						fig.x = evt.getX();
-                                		fig.y = evt.getY();
+				if ((this.w + this.x -  evt.getX())>0) {
+					if ((this.h + this.y -  evt.getY())>0) {
+						this.w += this.x -  evt.getX();
+                                		this.h += this.y - evt.getY();
+						this.x = evt.getX();
+                                		this.y = evt.getY();
 					}
 				}
 			break;
 
 			case 2:
-				if((evt.getX() - fig.x)>0) {
-					if ((fig.h+fig.y - evt.getY())>0) {
-						fig.w=evt.getX() - fig.x;
-						fig.h+=fig.y - evt.getY();
-                                		fig.y = evt.getY();
+				if((evt.getX() - this.x)>0) {
+					if ((this.h+this.y - evt.getY())>0) {
+						this.w=evt.getX() - this.x;
+						this.h+=this.y - evt.getY();
+                                		this.y = evt.getY();
 					}
 				}
 			break;
 
 			case 3:
-				if ((fig.w + fig.x -  evt.getX())>0) {
-					if((evt.getY()-fig.y)>0) {
-						fig.w += fig.x -  evt.getX();
-                                      		fig.h = evt.getY()-fig.y;
-						fig.x = evt.getX();
+				if ((this.w + this.x -  evt.getX())>0) {
+					if((evt.getY()-this.y)>0) {
+						this.w += this.x -  evt.getX();
+                                      		this.h = evt.getY()-this.y;
+						this.x = evt.getX();
 					}
 				}
 			break;
 
 			case 4:
-				if ((evt.getX()-fig.x)>0) {
-					if ((evt.getY()-fig.y)>0) {
-						fig.w =evt.getX()-fig.x ;
-                                      		fig.h = evt.getY()-fig.y;
+				if ((evt.getX()-this.x)>0) {
+					if ((evt.getY()-this.y)>0) {
+						this.w=evt.getX()-this.x ;
+                                      		this.h = evt.getY()-this.y;
 					}
                                 }
 			break;
 		}
 	}	
-    public Figure foco (Figure fig,MouseEvent evt) {
-        if (fig.x <= evt.getX() && evt.getX() <= fig.x + fig.w) { 
-		    if(fig.y <= evt.getY() && evt.getY() <= fig.y + fig.h) {
-			    return fig;
+    public Figure foco (MouseEvent evt,Figure fig) {
+        if (this.x <= evt.getX() && evt.getX() <= this.x + this.w) { 
+		    if(this.y <= evt.getY() && evt.getY() <= this.y + this.h) {
+			    return fig; 
 		    }
 	    }
 	return null;
     }
-    public int cantofig (Figure focus,Figure fig,MouseEvent evt) {
-	if (focus!=null) {
-		if (fig.x <= evt.getX() && evt.getX() <= fig.x + 5) { 
-            		if(fig.y <= evt.getY() && evt.getY() <= fig.y + 5) {
+    public int cantofig (MouseEvent evt) {
+		if (this.x <= evt.getX() && evt.getX() <= this.x + 5) { 
+            		if(this.y <= evt.getY() && evt.getY() <= this.y + 5) {
             			return 1;
         		}
 		}
-        	if (fig.x + fig.w - 5 <= evt.getX() && evt.getX() <= fig.x + fig.w) {
-            		if (fig.y <= evt.getY() && evt.getY() <= fig.y + 5) {
+        	if (this.x + this.w - 5 <= evt.getX() && evt.getX() <= this.x + this.w) {
+            		if (this.y <= evt.getY() && evt.getY() <= this.y + 5) {
             			return 2;
 			}
        		}
-        	if (fig.x <= evt.getX() && evt.getX() <= fig.x + 5) {
-            		if(fig.y + fig.h - 5 <= evt.getY() && evt.getY() <= fig.y + fig.h) {
+        	if (this.x <= evt.getX() && evt.getX() <= this.x + 5) {
+            		if(this.y + this.h - 5 <= evt.getY() && evt.getY() <= this.y + this.h) {
             			return 3;
 			}
         	}
-        	if (fig.x + fig.w - 5 <= evt.getX() && evt.getX() <= fig.x + fig.w) {
-            		if(fig.y + fig.h - 5 <= evt.getY() && evt.getY() <= fig.y + fig.h) {
+        	if (this.x + this.w - 5 <= evt.getX() && evt.getX() <= this.x + this.w) {
+            		if(this.y + this.h - 5 <= evt.getY() && evt.getY() <= this.y + this.h) {
             			return 4;
         		}
 		}
-	}
 	return 0;
     }
-    public void paintfocus (Graphics g,Figure focus,Figure fig) {
-            int x=fig.x;
-		        int y=fig.y;
-		        int w=fig.w;
-		        int h=fig.h;
-            if (fig==focus) {
-                    if(fig.w<0) {
+    public void paintfocus (Graphics g) {
+            int x=this.x;
+		        int y=this.y;
+		        int w=this.w;
+		        int h=this.h;
+          
+                    if(this.w<0) {
    	                 w=-w;
    	                 x-=w;
    	                }
-   	            if(fig.h<0) {
+   	            if(this.h<0) {
    	                 h=-h;
    	                 y-=h;
    	                }
    	                 g.setColor(Color.RED);
-    		        g.drawRect(fig.x, fig.y, fig.w, fig.h);   
-		        }
+    		        g.drawRect(x, y, w, h);   
     
     }
     public boolean clicked (int x, int y) {
         return (this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
     }
-    public abstract void paint (Graphics g) ;
+    public abstract void paint (Graphics g,boolean focused) ;
   } 
